b36cfad565e85fb4610ce927a8dc2c0a07ab7e52
Eduardo Vizcaino <evigra@hotmail.com>
2020-01-23 15:19:40 -0600

    Update position_form

--- solesgps_geofence/models/solesgps_geofence.py
+++ solesgps_geofence/models/solesgps_geofence.py
@@ -4,24 +4,10 @@ import requests
 from dateutil.relativedelta import relativedelta
 from odoo import api, fields, models, _
 
+
 class geofence(models.Model):
-    _name = "gpsmap.geofence"
-    _description = 'GPS Geofence'
-    _pointOnVertex=""
-    name = fields.Char('Name', size=75)
-    description = fields.Char('Description', size=150)
-    area = fields.Text('area')
-    attributes = fields.Text('Attributes')
-    points = fields.Text('Points')
-    color = fields.Selection([
-        ('green', 'Green'),
-        ('red', 'Red'),
-        ('blue', 'Blue'),
-        ('black', 'Black'),
-        ('grey', 'Grey'),
-        ('yellow', 'Yellow'),
-        ], 'Color', default='green', help='Color of geofence', required=True)
-    hidden = fields.Integer('Valido')
+    _inherit = "gpsmap.geofence"
+
         
 class geofence_device(models.Model):
     _name = "gpsmap.geofence_device"
